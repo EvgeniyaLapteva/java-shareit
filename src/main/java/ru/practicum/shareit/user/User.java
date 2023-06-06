fb9f45901df9c2c6f253c11bd35b22254d1a7543
EvgeniyaLapteva <evgeniyalaptev4@yandex.ru>
2023-06-06 14:41:43 +0700

    add classes

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.user;
 
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
 public class User {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotNull
+    @Email
+    private String email;
 }
