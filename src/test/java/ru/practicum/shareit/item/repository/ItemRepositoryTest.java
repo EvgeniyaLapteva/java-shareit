27401473ebed95b97c4c37bebadb9df2fa922c8c
EvgeniyaLapteva <evgeniyalaptev4@yandex.ru>
2023-07-16 10:35:25 +0700

    Add more Repository test

--- src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
@@ -16,7 +16,6 @@ import ru.practicum.shareit.user.repository.UserRepository;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
