9d5707d0c74acd4dd1f796e119d373293e4423c0
EvgeniyaLapteva <evgeniyalaptev4@yandex.ru>
2023-07-25 22:25:54 +0300

    Отправка на ревью №2

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.exception.model.ValidationException;
 import ru.practicum.shareit.exception.validation.ValidationMarker;
 import ru.practicum.shareit.user.dto.UserDto;
 
